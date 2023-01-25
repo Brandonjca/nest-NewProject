804873127dfa46fdfafeaf7d084a7156253d67cb
Aldair <bat.real@yavirac.edu.ec>
2023-01-25 08:57:01 -0500

    product

--- src/modules/ventas/entities/product.entity.ts
+++ src/modules/ventas/entities/product.entity.ts
@@ -1,6 +1,46 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+
 @Entity('products', {schema:'ventas'})
 
 export class ProductEntity{
-    @PrimaryGenerateColumn('sv')
+    @PrimaryGeneratedColumn('sv')
     id:number
+    @CreateDateColumn({
+        name:'Create_date',
+        type:'timestamp',
+        default:() => 'CURRENT_TIMESTAMP',
+
+    })
+    createAt:Date;
+    @UpdateDateColumn({
+        name:'update_date',
+        type:'timestamp',
+        default:() => 'CURRENT_TIMESTAMP',
+    })
+
+    updateAp:Date;
+    @DeleteDateColumn({
+        name:'delete_date',
+        type:'timestamp',
+        nullable:true,
+    })
+    deleteAp:Date;
+
+    //Columnas
+
+    @Column('varchar', {
+        name:'title',
+        comment:'nombre del producto',
+    })
+    title:string;
+    @Column('number', {
+        name:'price',
+        comment:'precio con 2 decimales del producto'
+    })
+    price:number;
+    @Column('text', {
+        name:'Description',
+        comment: 'Descripcion del producto'
+    })
+    description:string;
 }
